8b19c3f893a49bb8e3128ea39cf5005fe1b0b27a
QuasiXD <abhirajc08@gmail.com>
2026-02-21 13:39:08 +0530

    Static Clock

--- main.py
+++ main.py
@@ -6,12 +6,30 @@ class DigitalClock(QWidget):
 
     def __init__(self):
         super().__init__()
+        self.time_label = QLabel(self)
+        self.timer = QTimer(self)
         self.initUI()
 
     def initUI(self):
-        pass
+        self.setWindowTitle("Digital Clock")
+        self.setGeometry(400,250,300,100)
 
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.time_label)
+        self.setLayout(vbox)
 
+        self.time_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.time_label.setStyleSheet("font-size: 150px;"
+                                      "font-family: Arial;"
+                                      "color: hsl(111,100%,50%);")
+        self.setStyleSheet("background-color: black;")
+
+        self.update_time()
+
+    def update_time(self):
+        current_time = QTime.currentTime().toString("hh:mm:ss")
+        self.time_label.setText(current_time)
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     clock = DigitalClock()
